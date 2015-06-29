143510b947ebc58e6494e3221a98208f9edaf38b
Joerg Unbehauen <joerg@unbehauen.net>
2015-06-29 15:24:45 +0200

    registering the sql server components

---
+++ src/main/java/org/aksw/sparqlmap/core/db/impl/SqlServerConnector.java
@@ -0,0 +1,25 @@
+package org.aksw.sparqlmap.core.db.impl;
+
+import org.aksw.sparqlmap.core.db.Connector;
+
+public class SqlServerConnector extends Connector{
+  
+  public static String SQL_SEVER_NAME = "MSSQLSERVER";
+  public static String SQL_SERVER_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+
+  @Override
+  public String getDBName() {
+   
+    return SQL_SEVER_NAME;
+  }
+
+ 
+  
+  
+  @Override
+  public String getDriverClassString() {
+    return SQL_SERVER_DRIVER;
+  }
+  
+
+}
---
+++ src/main/java/org/aksw/sparqlmap/core/db/impl/SqlServerDataTypeHelper.java
@@ -0,0 +1,5 @@
+package org.aksw.sparqlmap.core.db.impl;
+
+public class SqlServerDataTypeHelper {
+
+}
